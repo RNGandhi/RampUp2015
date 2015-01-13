8638c37771d7eb555d61cf6b8d2b61b839b14a0f
ruta <ruta.gandhi.rg@gmail.com>
2015-01-12 20:13:25 -0500

    First File

---
+++ expense.rb
@@ -0,0 +1,21 @@
+
+#def calculate_bill_for_roommates(gas, electric, rent, roommates)
+#total = gas + electric + rent
+#division = total/roommates
+#endname
+#puts calculate_bill_for_roommates(93.11, 72.71, 3100, 4)
+
+def get_name_and_age
+	puts "type name"
+	name = gets
+
+	puts "age"
+	age = gets.to_i
+
+	puts name*age
+end
+	get_name_and_age
+
+
+
+
