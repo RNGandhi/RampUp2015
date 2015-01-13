1000e297e8d647314b18ea683bc4ec2877ce5fc5
ruta <ruta.gandhi.rg@gmail.com>
2015-01-12 21:24:18 -0500

    Trial

---
+++ game.rb
@@ -0,0 +1,13 @@
+puts "you are in a dark forest. You come to fork in the path.Which way do you go?"
+puts "North or East?"
+
+direction = gets.chomp
+
+if direction = "North"
+	puts "you travel to North.Cold winds will blow"
+
+	elseif direction = "East"
+		puts "You go east.Mountains loom in distance"
+	else
+		puts"You sit at the fork and consider the options"
+	end
